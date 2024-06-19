39c7b1c1d6c6d3e97aaeec665c8402c89e577a34
Carla <carla@unith.ai>
2024-06-19 21:44:33 +0000

    model form efs

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -22,7 +22,7 @@ dataset_paths = {
 
 model_paths = {
 	'stylegan_ffhq': 'pretrained_models/stylegan2-ffhq-config-f.pt',
-	'ir_se50': 'pretrained_models/model_ir_se50.pth',
+	'ir_se50': '/home/ubuntu/efs/data/users/carla/trainings/test_e4e_256/model/model_ir_se50.pth',
 	'shape_predictor': 'pretrained_models/shape_predictor_68_face_landmarks.dat',
 	'moco': 'pretrained_models/moco_v2_800ep_pretrain.pth'
 }
